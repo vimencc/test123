de233e41eb59c7ace0ff53f700a58fcb00e8b19d
vimen <>
2026-01-30 15:45:39 +0800

    123

---
+++ 123/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by insight on 30/1/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+               
+        
+        let view122 = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        
+        view122.backgroundColor = .yellow
+
+        self.view.addSubview(view122)
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+}
+
